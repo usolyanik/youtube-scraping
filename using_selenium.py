aca8f930066c797d4c0d2389569e9b497bd16937
yuriy <u.solyanik@yahoo.com>
2019-03-28 07:21:27 +0200

    result template

--- using_selenium.py
+++ using_selenium.py
@@ -6,21 +6,37 @@ import time
 import inspect
 
 from selenium import webdriver
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
+#from selenium.webdriver.support.wait import WebDriverWait
+#from selenium.webdriver.support import expected_conditions as EC
+#from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 from selenium.webdriver.common.keys import Keys
 
 from log import Log
 from debug import Debug
 
-def line_num():
-    print(inspect.currentframe().f_back.f_lineno)
-
 binary = FirefoxBinary('/usr/bin/firefox')
 url = 'https://www.youtube.com/watch?v=p9DWfW2JYs8'
 page_load_timeout = 120
+scrolling_timeout = 120
+
+views_class= 'view-count style-scope yt-view-count-renderer'
+comments_class = 'count-text style-scope ytd-comments-header-renderer'
+
+rex_views = views_class + r'.{150}'
+rex_comments = comments_class + r'.{150}'
+
+def line_num():
+    print(inspect.currentframe().f_back.f_lineno)
+    
+def get_views_num(html):
+    lst = re.findall(rex_views, html)
+    return lst[0]
+
+def get_comments_num(html):
+    lst = re.findall(rex_comments, html)
+    return lst[0]
+#########################################################################
 
 try:
             
@@ -30,79 +46,41 @@ try:
 
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-#    driver.execute_script('data.countText()')
-    
-    #views_class = 'view-count style-scope yt-view-count-renderer'
-    comments_class = 'count-text style-scope ytd-comments-header-renderer'
-    #comments_class = 'ytd-comments-header-renderer'
-    #for view_num in driver.find_elements_by_class_name(views_class):
-    #    print 'Number of views: ' + view_num.text.replace(' views', '')
-    
     source = None
-#    element = WebDriverWait(driver, page_load_timeout).until(
-#        EC.presence_of_element_located((By.CLASS_NAME, 
-#                                        comments_class)))
-    
-#    WebDriverWait(driver, page_load_timeout)
-#
-#    
-#    element = driver.find_element_by_class_name(comments_class)
-#    element.location_once_scrolled_into_view
-#    element.send_keys(Keys.END)
-    
-    SCROLL_PAUSE_TIME = 0.5
 
-    last_height = driver.execute_script("return document.body.scrollHeight")
-    
-    while True:
-
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(SCROLL_PAUSE_TIME)
-        new_height = driver.execute_script("return document.body.scrollHeight")
-        if new_height == last_height:
-            break
-        last_height = new_height
-    
-#    driver.execute_script('data.countText()')
-    
-    line_num()
     page = driver.find_element_by_tag_name('body')
     for i in range(10):
         page.send_keys(Keys.PAGE_DOWN)
-        time.sleep(3)
+        time.sleep(scrolling_timeout)
         source = driver.page_source
-        try:
-            element = driver.find_element_by_class_name(comments_class)
-            element.location_once_scrolled_into_view
-        except:
+        views_num = get_views_num(source)
+        comments_num = get_comments_num(source)
+        print(comments_num)
+        if 'text="[[data.count' in comments_num: 
             continue
         else:
             break
-    line_num()
-    
-    source = driver.page_source
-    
-#    for comment_num in driver.find_elements_by_class_name(comments_class):
-#        print u'Number of comments: ' + comment_num.text
+
 except:
     print('except')
     source = driver.page_source
-    Debug.print_exception_info()
+    Log.write(Debug.exception_info(), print_it=True)
 finally:
     try:
         print('finally')
         driver.quit()
     except:    
-        Debug.print_exception_info()
+        Log.write(Debug.exception_info(), print_it=True)
 
 #rex = comments_class + r'[~>]*?>([~<]*?)<'
 #arr = re.findall(rex, source)
 #pp(arr)
-line_num() 
+
 p = source.find(comments_class)
 print(source[p:p+150])
 
 
+#data.countText
 
 
 
