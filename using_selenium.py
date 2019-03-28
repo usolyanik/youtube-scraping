77352aec11188a0a911d1536ac87e6c641157ef4
yuriy <u.solyanik@yahoo.com>
2019-03-28 08:38:06 +0200

    results html

--- using_selenium.py
+++ using_selenium.py
@@ -6,9 +6,6 @@ import time
 import inspect
 
 from selenium import webdriver
-#from selenium.webdriver.support.wait import WebDriverWait
-#from selenium.webdriver.support import expected_conditions as EC
-#from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 from selenium.webdriver.common.keys import Keys
 
@@ -18,24 +15,26 @@ from debug import Debug
 binary = FirefoxBinary('/usr/bin/firefox')
 url = 'https://www.youtube.com/watch?v=p9DWfW2JYs8'
 page_load_timeout = 120
-scrolling_timeout = 120
+scrolling_timeout = 2
 
 views_class= 'view-count style-scope yt-view-count-renderer'
 comments_class = 'count-text style-scope ytd-comments-header-renderer'
 
-rex_views = views_class + r'.{150}'
-rex_comments = comments_class + r'.{150}'
+reg_expr_views = views_class + r'.{150}'
+reg_expr_comments = comments_class + r'.{150}'
 
 def line_num():
     print(inspect.currentframe().f_back.f_lineno)
     
-def get_views_num(html):
-    lst = re.findall(rex_views, html)
-    return lst[0]
-
-def get_comments_num(html):
-    lst = re.findall(rex_comments, html)
-    return lst[0]
+def get_value_html(reg_expr, html):
+    if not html:
+        return ''
+    lst = re.findall(reg_expr, html)
+    if len(lst) > 0:
+        return lst[0]
+    else:
+        return ''
+    
 #########################################################################
 
 try:
@@ -47,16 +46,31 @@ try:
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
     source = None
-
-    page = driver.find_element_by_tag_name('body')
+    page = None
+    
+    i = 0
+    while not page:
+        print(i)
+        i += 1
+        page = driver.find_element_by_tag_name('body')
+        time.sleep(1)
+        
     for i in range(10):
-        page.send_keys(Keys.PAGE_DOWN)
-        time.sleep(scrolling_timeout)
+        
+        print(i)
+        page.send_keys(Keys.PAGE_DOWN)  # scroll down
+        time.sleep(scrolling_timeout)   # until comments number is generated
+        
         source = driver.page_source
-        views_num = get_views_num(source)
-        comments_num = get_comments_num(source)
+        views_num = get_value_html(reg_expr_views, source)
+        comments_num = get_value_html(reg_expr_comments, source)
+        
+        print('################')
+        print(views_num)
+        print('-----------------')
         print(comments_num)
-        if 'text="[[data.count' in comments_num: 
+        print('-----------------')
+        if ('text="[[data.countText' in comments_num) or not comments_num.strip(): 
             continue
         else:
             break
@@ -66,23 +80,19 @@ except:
     source = driver.page_source
     Log.write(Debug.exception_info(), print_it=True)
 finally:
+    print('finally')
     try:
-        print('finally')
         driver.quit()
     except:    
         Log.write(Debug.exception_info(), print_it=True)
 
-#rex = comments_class + r'[~>]*?>([~<]*?)<'
-#arr = re.findall(rex, source)
-#pp(arr)
-
-p = source.find(comments_class)
-print(source[p:p+150])
-
-
-#data.countText
-
-
+pos = source.find(views_class)
+html_views = source[pos:pos+150]
+print(html_views)
+print('..................................')
+pos = source.find(comments_class)
+html_comments = source[pos:pos+150]
+print(html_comments)
 
 
 
